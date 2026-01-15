7509355e94ee3bb28282a143fc4fbf7b3950170f
chubarovOleksandr <sasha.chubarov.2022@gmail.com>
2026-01-15 23:04:38 +0200

    cooming soon page start return on prev page (location)

--- src/pages/ComingSoon/ComingSoonPage.tsx
+++ src/pages/ComingSoon/ComingSoonPage.tsx
@@ -1,16 +1,11 @@
 import { useNavigate } from 'react-router-dom';
 import { Button, Flex, Text } from '@radix-ui/themes';
-import { getAuth } from 'firebase/auth';
-
-import { RoutesEnum } from '@enums/routes';
-import { isExist } from '@utils/format';
 
 export const ComingSoonPage = () => {
   const navigate = useNavigate();
-  const { currentUser } = getAuth();
 
-  const handleReturn = () => {
-    navigate(isExist(currentUser) ? RoutesEnum.Main : RoutesEnum.SignIn);
+  const goBack = () => {
+    navigate(-1);
   };
 
   return (
@@ -22,7 +17,7 @@ export const ComingSoonPage = () => {
       gap={'4'}
     >
       <Text size={'5'}>Страница находится в разработке. Скоро она будет доступна!</Text>
-      <Button color="gray" highContrast onClick={handleReturn}>
+      <Button color="gray" highContrast onClick={goBack}>
         Вернуться
       </Button>
     </Flex>
