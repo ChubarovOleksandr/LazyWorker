838a0270e37d43267466a82191d1cb1c82230559
ChubarovOleksandr <o.chubarov@finik.pro>
2026-06-10 13:30:51 +0300

    remove unused hooks

--- src/layouts/NavigationLayout/hooks/useSchedule.ts
+++
@@ -1,8 +0,0 @@
-import { useEffect } from 'react';
-
-import { scheduleStore } from '@store/scheduleStore';
-
-export const useSchedule = () =>
-  useEffect(() => {
-    void scheduleStore.loadSchedule();
-  }, []);
--- src/layouts/NavigationLayout/hooks/useUserSettings.ts
+++
@@ -1,8 +0,0 @@
-import { useEffect } from 'react';
-
-import { settingsStore } from '@store/settingsStore.ts';
-
-export const useUserSettings = () =>
-  useEffect(() => {
-    void settingsStore.loadSettings();
-  }, []);
